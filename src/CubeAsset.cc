72f84d0b974d2135fec7d780bf318d125192601c
Aidan Delaney <aidan@ontologyengineering.org>
2014-09-18 13:32:04 +0100

    Removed camera (no need to introduce this complexity at this stage)

--- src/CubeAsset.cc
+++ src/CubeAsset.cc
@@ -64,7 +64,6 @@ void CubeAsset::Draw(GLuint program_token) {
     std::vector<char> errorLog(maxLength);
     glGetProgramInfoLog(program_token, maxLength, &maxLength, &errorLog[0]);
 
-    //Provide the infolog in whatever manor you deem best.
     std::cerr << "Invalid program " << program_token << " with error code " << validation_ok << std::endl;
     for(auto c: errorLog) {
       std::cerr << c;
@@ -72,20 +71,11 @@ void CubeAsset::Draw(GLuint program_token) {
     exit(-1);
   }
 
-  glm::mat4 camera_matrix = glm::lookAt(
-                                        glm::vec3(0,0,3), // Camera is at (0,0,3), in World Space
-                                        glm::vec3(0,0,0), // and looks at the origin
-                                        glm::vec3(0,1,0)  // Head is up
-                                        );
-
   GLuint position_attrib = glGetAttribLocation(program_token, "position");
-  GLuint camera_attrib = glGetUniformLocation(program_token, "camera");
   checkGLError();
 
   glUseProgram(program_token);
   checkGLError();
-
-  glUniformMatrix4fv(camera_attrib, 1, GL_FALSE, &camera_matrix[0][0]);
 
   // use the previously transferred buffer as the vertex array.  This way
   // we transfer the buffer once -- at construction -- not on every frame.
