5ffd3fdbfe2ecba02cf94157e82529d411fffc7d
Aidan Delaney <aidan@ontologyengineering.org>
2014-09-18 10:34:11 +0100

    Changed background colour and window title.

--- src/main.cc
+++ src/main.cc
@@ -35,7 +35,7 @@ struct SDLWindowDeleter {
 };
 
 void Draw(const std::shared_ptr<SDL_Window> window, const std::shared_ptr<GameWorld> game_world) {
-  glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+  glClearColor(0.0f, 1.0f, 0.0f, 1.0f);
   glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
 
   game_world->Draw();
@@ -70,7 +70,7 @@ std::shared_ptr<SDL_Window> InitWorld() {
   atexit(SDL_Quit);
 
   // Create a new window with an OpenGL surface
-  _window = SDL_CreateWindow("Translate Shader Example"
+  _window = SDL_CreateWindow("Shader Example"
                              , SDL_WINDOWPOS_CENTERED
                              , SDL_WINDOWPOS_CENTERED
                              , width
